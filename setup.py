99c7c63ce9aa6ef5cb4ddf4834f044e0f238f49d
ParsaAlizadeh <parsa.alizadeh1@gmail.com>
2020-11-25 20:02:49 +0330

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+with open("requirements.txt", "r") as file:
+    install_requires = file.readlines()
+
+setup(
+    name="fiml",
+    description="VCS-like program for managing episodes and subtitles, and watching through mpv",
+    version="0.0.1",
+    author="Parsa Alizadeh",
+    license="MIT",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": ["fiml=fiml:main"]
+    },
+    install_requires=install_requires,
+)
