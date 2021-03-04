c2ec6520c091cf93a4ffae42f00e4e73410ca66c
ParsaAlizadeh <parsa.alizadeh1@gmail.com>
2021-03-04 20:15:35 +0330

    Upgrade fiml
    * set config file to be json
    * search for all files inside working directory
    * better document

--- fiml/__init__.py
+++ fiml/__init__.py
@@ -1,3 +1,6 @@
+#! /usr/bin/env python3
+
+import json
 import logging
 import os
 import subprocess
@@ -25,6 +28,7 @@ class Video:
         self.sub = sub_file
 
     def watch(self) -> int:
+        ''' call watch process '''
         logging.info(f"Watching {self.video}...")
         command = ["mpv", self.video, "--no-terminal"]
         if self.sub:
@@ -34,75 +38,53 @@ class Video:
 
     @staticmethod
     def is_video(filename: str) -> bool:
+        ''' check if video '''
         return any(filename.endswith(ext) for ext in (".mp4", ".mkv"))
 
     @staticmethod
     def is_sub(filename: str) -> bool:
+        ''' check if sub '''
         return filename.endswith("srt")
 
     @classmethod
     def find_all(cls, files: List[str]):
+        ''' find and match videos and subs '''
+        # sort lexographically
         videos = sorted(filter(cls.is_video, files))
         subs = sorted(filter(cls.is_sub, files))
         subs = resize(subs, len(videos))
         return [Video(i, vid, sub) for i, (vid, sub) in enumerate(zip(videos, subs))]
 
 
-class Parser:
-    def __init__(self, parser: ConfigParser):
-        self.parser = parser
-        self.counter = int(parser["main"]["counter"])
+class Context:
+    def __init__(self):
+        self.data = {}
+        self.counter = 0
+    
+    @property
+    def counter(self):
+        return self.data['counter']
+    
+    @counter.setter
+    def counter(self, x: int):
+        self.data['counter'] = x
 
-    def write(self, filename: str):
-        self.parser["main"] = {"counter": str(self.counter)}
-        with open(filename, "w") as file:
-            self.parser.write(file)
-
-    @classmethod
-    def read(cls, filename: str):
+    def read_file(self, filename: str):
+        ''' load from file '''
         if not os.path.isfile(filename):
-            parser = cls.create_default()
-        else:
-            parser = ConfigParser()
-            with open(filename, "r") as file:
-                parser.read_file(file)
-        return cls(parser)
+            return self
+        with open(filename, 'r') as file:
+            self.data = json.load(file)
+        return self
 
-    @staticmethod
-    def create_default() -> ConfigParser:
-        logging.info("Initialize configs for first time")
-        parser = ConfigParser()
-        parser["main"] = {"counter": "0"}
-        return parser
-
-
-def main():
-    parser = Parser.read(".fiml")
-    all_files = os.listdir()
-    videos = Video.find_all(all_files)
-    if parser.counter >= len(videos):
-        logging.info("You watched all episodes already :)")
-        parser.counter = len(videos)
-
-    options = [vid.video for vid in videos]
-    options.append("exit")
-    current = choose_option(message="Which episode?",
-                            options=options,
-                            default=parser.counter)
-    if current == len(videos):
-        logging.info("Ok, no episodes for now")
-        return
-    if parser.counter != current and ask_confirm("Reset counter to this episode?"):
-        parser.counter = current
-
-    videos[current].watch()
-
-    if current == parser.counter and ask_confirm("Did you watch this episode completely?"):
-        parser.counter += 1
-    parser.write(".fiml")
+    def write_file(self, filename: str):
+        ''' write to file '''
+        with open(filename, 'w') as file:
+            json.dump(self.data, file)
 
 
 def choose_option(message: str, options: List[str], default: int = 0) -> int:
+    ''' option prompt on terminal '''
     answer = inquirer.prompt([
         inquirer.List("list",
                       message=message,
@@ -113,11 +95,59 @@ def choose_option(message: str, options: List[str], default: int = 0) -> int:
 
 
 def ask_confirm(message: str) -> bool:
+    ''' yes/no question on terminal '''
     answer = inquirer.prompt([
         inquirer.Confirm("confirm",
                          message=message)
     ])
     return answer["confirm"]
+
+def list_files(root):
+    ''' list all files inside root '''
+    result = []
+    for root, _, files in os.walk(root):
+        for f in files:
+            result.append(os.path.join(root, f))
+    return result
+
+
+def main():
+    ctx = Context().read_file('.fiml')
+
+    # explore whole directory
+    all_files = list_files('.')
+
+    # find videos and match subs to them
+    videos = Video.find_all(all_files)
+    
+    # if no new episode
+    if ctx.counter >= len(videos):
+        logging.info("You watched all episodes already :)")
+        ctx.counter = len(videos)
+
+    # prompt to choose a video
+    options = [vid.video for vid in videos]
+    options.append("exit")
+    current = choose_option(message="Which episode?",
+                            options=options,
+                            default=ctx.counter)
+
+    # exit option
+    if current == len(videos):
+        logging.info("Ok, no episodes for now")
+        return
+    
+    # not default option
+    if ctx.counter != current and ask_confirm("Reset counter to this episode?"):
+        ctx.counter = current
+
+    videos[current].watch()
+
+    # default option and increament
+    if current == ctx.counter and ask_confirm("Did you watch this episode completely?"):
+        ctx.counter += 1
+    
+    ctx.write_file(".fiml")
 
 
 if __name__ == "__main__":
